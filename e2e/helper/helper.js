f7f5bd9066c381a34b54c93565bf0fab94883c9f
TestesJn2 <priscila@jn2.com.br>
2018-02-15 17:20:30 -0200

    refatoração de código

--- e2e/helper/helper.js
+++ e2e/helper/helper.js
@@ -1,10 +1,30 @@
 //helper.js
 
 'use strict';
-var EC = protractor.ExpectedConditions;
+var	EC = protractor.ExpectedConditions;
 
-module.exports = {
-	waitElementVisibility : function (element) {
-		browser.wait(EC.visibilityOf(element), 12000);
-	}
+var	Helper	=	function()	{};
+
+var waitElementVisibility	= function(element)	{
+	this.element = element;		
+	browser.wait(EC.visibilityOf(element),	9000); 
 };
+
+Helper.prototype.waitElementVisibility = function() {};
+
+var selecionarElemento = function(element)	{	
+	waitElementVisibility.call(element);
+	element.sendKeys(protractor.Key.ENTER); 
+};
+
+Helper.prototype.selecionarElemento = function() {};
+
+var editarElemento = function (element, txt) {
+	waitElementVisibility.call(element);
+	this.texto = txt;
+	element.sendKeys(texto);
+};
+
+Helper.prototype.editarElemento = function() {};
+
+module.exports	=	new Helper();
