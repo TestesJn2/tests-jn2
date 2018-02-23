1f249c5629824c6e6d6301de08bcec7c8c625d9b
TestesJn2 <priscila@jn2.com.br>
2018-02-23 14:40:17 -0300

    criação de métodos gerais  e que serão utilizados com prototype

--- e2e/helper/helper.js
+++ e2e/helper/helper.js
@@ -19,15 +19,27 @@ var  alertElementPresent = function(element) {
 	browser.wait(EC.alertIsPresent(element), 9000);
 };
 
+//
 var elementToBeClickable = function(element) {
 	this.element = element;
 	browser.wait(EC.elementToBeClickable(element), 9000);
 };
 
-// Métodos Gerais
+//Verifica se radiobutton, ou um checkbox foi seleciado antes de avançar para os próximos testes
+var elementToBeSelected = function(element) {
+	this.element = element;
+	browser.wait(EC.elementToBeSelected(element), 9000);
+}
+
+// Métodos Gerais de Interação
 Helper.prototype.selecionarElementoClick = function(element)	{
 	elementToBeClickable.call(this, element);
 	element.click();
+};
+
+Helper.prototype.selecionarElementoENTER = function(element) {
+	elementToBeClickable.call(this, element);
+	element.sendKeys(protractor.Key.ENTER);
 };
 
 Helper.prototype.informarTextoElemento = function (element, txt) {
@@ -36,8 +48,19 @@ Helper.prototype.informarTextoElemento = function (element, txt) {
 	element.clear().sendKeys(txt);
 };
 
-Helper.prototype.selecionarRadioButton = function (element) {
-
+Helper.prototype.selecionarRadioButtonCheckbox = function (element) {
+	waitElementVisibility.call(this, element);
+	var teste = element.checked;
+	element.isSelected().then(function (statusElement) {
+		if (statusElement === true) {
+			console.log ('nenhuma ação precisa ser realizada, opção já está selecionada.');
+		} else {
+			element.checked = true;
+			elementToBeSelected.call(this, element);
+			console.log('Foi utilizado checked = true. Status do radioButton: ' + statusElement);
+		};
+	});
+	browser.driver.sleep(2000);
 };
 
 Helper.prototype.selecionarElementoIFrame = function (idIFrame, Element) {
