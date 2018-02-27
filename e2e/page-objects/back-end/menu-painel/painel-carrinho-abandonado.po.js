65c7e7a0c8792adc954f553ba9785136ce2ab9f0
TestesJn2 <priscila@jn2.com.br>
2018-02-27 10:25:17 -0300

    encapsular elementos da tela Painel Carrinho Abandonado

--- e2e/page-objects/back-end/menu-painel/painel-carrinho-abandonado.po.js
+++ e2e/page-objects/back-end/menu-painel/painel-carrinho-abandonado.po.js
@@ -5,7 +5,15 @@ var helper = require('../../helper/helper.js');
 
 var painelCarrinhoAbandonadoPage = function() {
 
-	//Elementos
+	this.tituloCarrinhoAbandonado = element(by.cssContainingText('h3', 'Painel de Carrinho Abandonado (Ebizmarts)'));
+	this.tituloReceitaGerada = element(by.cssContainingText('h4', 'Receita Gerada ao Longo da Vida'));
+	this.tituloMedidasPedido = element(by.cssContainingText('h4', 'Média de Pedidos'));
+	this.tituloReceita = element(by.cssContainingText('h4', 'Receita'));
+	this.tituloImpostos = element(by.cssContainingText('h4', 'Impostos'));
+	this.tituloEnvios = element(by.cssContainingText('h4', 'Envios'));
+	this.tituloPedidos = element(by.cssContainingText('h4', 'Pedidos'));
+	this.tituloTaxa = element(by.cssContainingText('h4', 'Taxa'));
+	this.labelPrecos = element(by.css('.price'));
 	
 };
 
